bc5dfbff7f83ad050b88e8f33efdeabc576b7960
march-dave <ysangy@gmail.com>
2018-06-03 13:16:31 -0400

    store

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import FilterList from "./FilterList.js";
+import { store } from "redux";
 import { Provider, connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <Provider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
-          <FilterList />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div>
+            <FilterList />
+          </div>
         </div>
-      </div>
       </Provider>
     );
   }
